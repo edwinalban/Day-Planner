c9ff47e6b4464d48e1bb5add4da6e76d4ac6dd6d
Yachtzee <edwinalban@gmail.com>
2023-05-28 14:35:33 -0700

    updated if statement in compareHour function in order to compare current time block hour in array to current hour and add classes to textareas appropriately

--- assets/script.js
+++ assets/script.js
@@ -8,18 +8,12 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
+$('.saveBtn').on('click', saveAppointment())
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
   todaysDate();
  
   displayTimeBlocks();
@@ -132,19 +126,14 @@ function displayTimeBlocks() {
 
 function compareHour() {
   var currentHour = dayjs().format('HH');
-  console.log(currentHour);
   dailyTimeBlocks.forEach(function(block) {
-    var hour = $('dailyTimeBlocks[block].hour');
-    console.log(block.hour);
-      if (hour < currentHour) {
+      if (block.hour < currentHour) {
         block.textarea.addClass('past');
-      } else if (hour > currentHour) {
+      } else if (block.hour > currentHour) {
         block.textarea.addClass('future')
       } else {
         block.textarea.addClass('present')
       };
-      console.log(hour > currentHour);
-      console.log(hour < currentHour);
   });
 };
 
